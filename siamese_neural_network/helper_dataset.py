cb95895d8d7b7d36f635c538bfe4e04716ff3c55
Shazzad Hasan <shazzadraihan@gmail.com>
2022-08-03 20:11:12 +0600

    updated typo

--- siamese_neural_network/helper_dataset.py
+++ siamese_neural_network/helper_dataset.py
@@ -58,49 +58,51 @@ class OmniglotDataset(Dataset):
             img1 = self.transform(img1)
             img2 = self.transform(img2)
   
-    return img1, img2, label
+        return img1, img2, label
 
 
 class nWayOneShotValidSet(Dataset):
-  def __init__(self, categories, root_dir, data_size, n_way, transform=None):
-    self.categories = categories
-    self.root_dir = root_dir
-    self.data_size = data_size 
-    self.n_way = n_way 
-    self.transform = transform 
+    def __init__(self, categories, root_dir, data_size, n_way, transform=None):
+        self.categories = categories
+        self.root_dir = root_dir
+        self.data_size = data_size 
+        self.n_way = n_way 
+        self.transform = transform 
+    
+    def __len__(self):
+        return self.data_size 
   
-  def __len__(self):
-    return self.data_size 
+    def __getitem__(self, idx):
+        category = random.choice(categories)
+        character = random.choice(category[1])
+        img_dir = root_dir + category[0] + "/" + character
+        img_name = random.choice(os.listdir(img_dir)) 
+        main_img = Image.open(img_dir + "/" + img_name)
+        if self.transform:
+            main_img = self.transform(main_img)
+    
+        test_set = []
+        label = np.random.randint(self.n_way)
+        label = torch.from_numpy(np.array([label], dtype=int))
+        for i in range(self.n_way):
+            test_img_dir = img_dir 
+            test_img_name = ""
+            # find a random image from the from the same set as the main
+            if i == label:
+                test_img_name = random.choice(os.listdir(img_dir))
+            else:
+                test_category = random.choice(categories)
+                test_character = random.choice(test_category[1])
+                test_img_dir = root_dir + test_category[0] + "/" + test_character 
+                while test_img_dir == img_dir:
+                    test_img_dir = root_dir + test_category[0] + "/" + test_character 
+                test_img_name = random.choice(os.listdir(test_img_dir))
+                
+            test_img = Image.open(test_img_dir + "/" + test_img_name)
 
-  def __getitem__(self, idx):
-    category = random.choice(categories)
-    character = random.choice(category[1])
-    img_dir = root_dir + category[0] + "/" + character
-    img_name = random.choice(os.listdir(img_dir)) 
-    main_img = Image.open(img_dir + "/" + img_name)
-    if self.transform:
-      main_img = self.transform(main_img)
-
-    test_set = []
-    label = np.random.randint(self.n_way)
-    label = torch.from_numpy(np.array([label], dtype=int))
-    for i in range(self.n_way):
-      test_img_dir = img_dir 
-      test_img_name = ""
-      # find a random image from the from the same set as the main
-      if i == label:
-        test_img_name = random.choice(os.listdir(img_dir))
-      else:
-        test_category = random.choice(categories)
-        test_character = random.choice(test_category[1])
-        test_img_dir = root_dir + test_category[0] + "/" + test_character 
-        while test_img_dir == img_dir:
-          test_img_dir = root_dir + test_category[0] + "/" + test_character 
-        test_img_name = random.choice(os.listdir(test_img_dir))
-      test_img = Image.open(test_img_dir + "/" + test_img_name)
-      if self.transform:
-        test_img = self.transform(test_img)
-      test_set.append(test_img)
+            if self.transform:
+                test_img = self.transform(test_img)
+            test_set.append(test_img)
 
     return main_img, test_set, label 
 
